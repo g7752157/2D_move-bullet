d8d14fcf233ac332d0fdc8f0c07aedd6ed28bf43
g7752157 <54761463+g7752157@users.noreply.github.com>
2019-11-10 17:00:40 +0800

    res

---
+++ 2D_move&bullet/Assets/Respawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+    public Vector3[] pos = new Vector3[8];
+    public GameObject[] EnemyList = new GameObject[10];
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 up = Vector2.up;
+        Vector2 rot =  Quaternion.Euler(0, 0, 30)* up ;
+        timer =0;
+        LoadEnemy();
+        for (int i = 0; i <= 7; i++)
+        {
+            pos[i]=  gameObject.transform.GetChild(i).gameObject.transform.position;
+        }
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void LoadEnemy()
+    {
+        EnemyList[0] = Resources.Load("Enemy") as GameObject;
+        EnemyList[1] = Resources.Load("GroupEnemyFrame") as GameObject;
+        EnemyList[2] = Resources.Load("GroundEnemy") as GameObject;
+        EnemyList[3] = Resources.Load("ShieldEnemy") as GameObject;
+
+    }
+}
