410f50c53ff9efff9f9d71a33943f6024283240e
g7752157 <54761463+g7752157@users.noreply.github.com>
2019-11-24 08:42:25 +0800

    1124

--- 2D_move&bullet/Assets/Respawner.cs
+++ 2D_move&bullet/Assets/Respawner.cs
@@ -7,9 +7,16 @@ public class Respawner : MonoBehaviour
     public Vector3[] pos = new Vector3[8];
     public GameObject[] EnemyList = new GameObject[10];
     public float timer;
+    public int wave, level;
+    //level-wave-time-positon-enemy-element-fin
+    public int[,] respawn = { { 1,1,5,0,0,0,0},{ 1,1,10,3,0,0,0} };
+    public int[] LoadLevel,LoadWave,LoadTime,LoadPosition,LoadEnemy,LoadElement,LoadFinish;
+    
     // Start is called before the first frame update
     void Start()
     {
+        wave = 1;
+        level = 1;
         Vector2 up = Vector2.up;
         Vector2 rot =  Quaternion.Euler(0, 0, 30)* up ;
         timer =0;
@@ -18,6 +25,7 @@ public class Respawner : MonoBehaviour
         {
             pos[i]=  gameObject.transform.GetChild(i).gameObject.transform.position;
         }
+        
     }
 
     
@@ -34,5 +42,14 @@ public class Respawner : MonoBehaviour
         EnemyList[2] = Resources.Load("GroundEnemy") as GameObject;
         EnemyList[3] = Resources.Load("ShieldEnemy") as GameObject;
 
+    }
+
+    void Respawn(int level, int wave)
+    {
+       for(int i=0;i<respawn.GetLength(0)-1 ;i++  )
+        {
+            
+        }
+       
     }
 }
