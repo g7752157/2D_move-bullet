71fdc5e4289ae74285c6d2dad91aa750819c6484
g7752157 <54761463+g7752157@users.noreply.github.com>
2019-11-24 17:32:19 +0800

    AI

--- 2D_move&bullet/Assets/Respawner.cs
+++ 2D_move&bullet/Assets/Respawner.cs
@@ -10,11 +10,13 @@ public class Respawner : MonoBehaviour
     public int wave, level;
     //level-wave-time-positon-enemy-element-fin
     public int[,] respawn = { { 1,1,5,0,0,0,0},{ 1,1,10,3,0,0,0} };
-    public int[] LoadLevel,LoadWave,LoadTime,LoadPosition,LoadEnemy,LoadElement,LoadFinish;
+    public int[] LoadLevel,LoadWave,LoadTime,LoadPosition,LoadEnemyType,LoadElement,LoadFinish;
+    public int count;
     
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         wave = 1;
         level = 1;
         Vector2 up = Vector2.up;
@@ -33,6 +35,8 @@ public class Respawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        RespawnReady();
+        Respawn();
     }
 
     void LoadEnemy()
@@ -44,12 +48,80 @@ public class Respawner : MonoBehaviour
 
     }
 
-    void Respawn(int level, int wave)
+    void RespawnReady()
     {
-       for(int i=0;i<respawn.GetLength(0)-1 ;i++  )
+        
+        while (count <= 0)
+        {
+            for (int i = 0; i < respawn.GetLength(0) ; i++)
+            {
+                if (respawn[i, 0] == level && respawn[i, 1] == wave)
+                {
+                    count++;
+                    Debug.Log(count);
+                }
+
+            }
+            LoadRespwn(count);
+            int loadnum = 0;
+            for (int i = 0; i < respawn.GetLength(0); i++)
+            {
+                if (respawn[i, 0] == level && respawn[i, 1] == wave)
+                {
+
+
+                    LoadLevel[loadnum] = respawn[i, 0];
+                    LoadWave[loadnum] = respawn[i, 1];
+                    LoadTime[loadnum] = respawn[i, 2];
+                    LoadPosition[loadnum] = respawn[i, 3];
+                    LoadEnemyType[loadnum] = respawn[i, 4];
+                    LoadElement[loadnum] = respawn[i, 5];
+                    LoadFinish[loadnum] = respawn[i, 6];
+                    loadnum++;
+
+                }
+            }
+        }
+
+
+    }
+    void LoadRespwn(int c)
+    {
+        for (int i = 0; i < c; i++)
         {
             
+                LoadLevel = new int[c];
+                LoadWave = new int[c];
+                LoadTime = new int[c];
+                LoadPosition = new int[c];
+                LoadEnemyType = new int[c];
+                LoadElement = new int[c];
+                LoadFinish = new int[c];
+                    
         }
+
+        
+        
+            
+
+        
+    }
+    void Respawn()
+    {
        
+        
+            for (int i = 0; i < LoadTime.Length; i++)
+            {
+                if (LoadTime[i]<timer && LoadFinish[i]<=0)
+                {
+                    
+                    Instantiate(EnemyList[LoadEnemyType[i]], pos[LoadPosition[i]], new Quaternion(0,0,0,0));
+                    Debug.Log(i);
+                    Debug.Log("Respawn!");
+                    LoadFinish[i]++;
+                    //count--;
+                }
+            }
+        
     }
 }
